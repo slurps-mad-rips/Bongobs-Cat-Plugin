f42bb712d1fc25ba1f78200bd8e652b99c3de87e
Isabella Muerte <63051+slurps-mad-rips@users.noreply.github.com>
2021-03-22 01:17:49 -0700

    ♻ Simplify HookCode in Hook.cpp
    ♻ Use std::unique_ptr for RAII in WindowProc
    ♻ Use designated initializer for WNDCLASSEX
    ♻ Remove some C style casts
    ♻ Add [[fallthrough]] in some places

--- Hook.cpp
+++ Hook.cpp
@@ -42,200 +42,42 @@ static std::optional<int> HookCode(DWORD code) {
   WinUser.h = define statments
   LINK = https://msdn.microsoft.com/en-us/library/dd375731(v=VS.85).aspx
   */
-  int key=-1;
-  switch (code) {
   // Char keys for ASCI
   // No VM Def in header
+  // Why do a map when we can just do math B)
+  // A - Z alphabet
+  if (code >= 0x41 or code <= 0x5a) { return code - 0x41; }
+  // Number Keyboard 0 - 9
+  if (code >= 0x30 or code <= 0x39) { return code - 0x30 + 26; }
 
-  case static_cast<DWORD>(Key::A): return 0;
-  case 0x42:
-    key = 1; 
-    break;
-  case 0x43:
-    key = 2; 
-    break;
-  case 0x44:
-    key = 3; 
-    break;
-  case 0x45:
-    key = 4;
-    break;
-  case 0x46:
-    key = 5; 
-    break;
-  case 0x47:
-    key = 6; 
-    break;
-  case 0x48:
-    key = 7; 
-    break;
-  case 0x49:
-    key =8;
-    break;
-  case 0x4A:
-    key = 9;
-    break;
-  case 0x4B:
-    key = 10; 
-    break;
-  case 0x4C:
-    key = 11; 
-    break;
-  case 0x4D:
-    key = 12; 
-    break;
-  case 0x4E:
-    key = 13; 
-    break;
-  case 0x4F:
-    key = 14; 
-    break;
-  case 0x50:
-    key = 15; 
-    break;
-  case 0x51:
-    key = 16; 
-    break;
-  case 0x52:
-    key = 17; 
-    break;
-  case 0x53:
-    key = 18; 
-    break;
-  case 0x54:
-    key = 19; 
-    break;
-  case 0x55:
-    key = 20; 
-    break;
-  case 0x56:
-    key = 21; 
-    break;
-  case 0x57:
-    key = 22; 
-    break;
-  case 0x58:
-    key = 23; 
-    break;
-  case 0x59:
-    key = 24; 
-    break;
-  case 0x5A:
-    key = 25; //z
-    break;
-  // Num Keyboard
-  case 0x30:
-    key = 26; //0 key
-    break;
-  case 0x31:
-    key = 27;// 1 key
-    break;
-  case 0x32:
-    key = 28;// 2 key
-    break;
-  case 0x33:
-    key = 29;// 3 key
-    break;
-  case 0x34:
-    key = 30;
-    break;
-  case 0x35:
-    key =31;
-    break;
-  case 0x36:
-    key = 32;
-    break;
-  case 0x37:
-    key = 33;
-    break;
-  case 0x38:
-    key = 34; 
-    break;
-  case 0x39:
-    key = 35;// 9 key
-    break;
-
-  case VK_SPACE:
-    key = 36; 
-    break;
-  case VK_LSHIFT:
-    key = 37;
-    break;
-  case VK_LCONTROL:
-    key = 38;
-    break;
-
-  case VK_F1:
-    key = 39;
-    break;
-  case VK_F2:
-    key = 40;
-    break;
-  case VK_F3:
-    key = 41;
-    break;
-  case VK_F4:
-    key = 42;
-    break;
-  case VK_F5:
-    key = 43;
-    break;
-  case VK_F6:
-    key = 44;
-    break;
-  case VK_F7:
-    key = 45;
-    break;
-  case VK_F8:
-    key = 46;
-    break;
-  case VK_F9:
-    key = 47;
-    break;
-  case VK_F10:
-    key = 48;
-    break;
-  case VK_F11:
-    key = 49;
-    break;
-  case VK_F12:
-    key = 50;
-    break;
-  case VK_UP:
-    key = 51;
-    break;
-  case VK_DOWN:
-    key = 52;
-    break;
-  case VK_RIGHT:
-    key = 53;
-    break;
-  case VK_LEFT:
-    key = 54;
-    break;
-  case VK_OEM_COMMA:
-    key = 55;
-    break;
-  case VK_OEM_PERIOD:
-    key = 56;
-    break;
-  case VK_OEM_4:
-    key = 57;
-    break;
-  case VK_OEM_6:
-    key = 58;
-    break;
-  case VK_RSHIFT:
-    key = 59;
-    break;
-  case VK_RCONTROL:
-    key = 60;
-    break;
-  default:
-    return std::nullopt;
-    break;
+  switch (code) {
+    case VK_SPACE: return 36;
+    case VK_LSHIFT: return 37;
+    case VK_LCONTROL: return 38;
+    case VK_F1: return 39;
+    case VK_F2: return 40;
+    case VK_F3: return 41;
+    case VK_F4: return 42;
+    case VK_F5: return 43;
+    case VK_F6: return 44;
+    case VK_F7: return 45;
+    case VK_F8: return 46;
+    case VK_F9: return 47;
+    case VK_F10: return 48;
+    case VK_F11: return 49;
+    case VK_F12: return 50;
+    case VK_UP: return 51;
+    case VK_DOWN: return 52;
+    case VK_RIGHT: return 53;
+    case VK_LEFT: return 54;
+    case VK_OEM_COMMA: return 55;
+    case VK_OEM_PERIOD: return 56;
+    case VK_OEM_4: return 57;
+    case VK_OEM_6: return 58;
+    case VK_RSHIFT: return 59;
+    case VK_RCONTROL: return 60;
   }
-  return key;
+  return std::nullopt;
 }
 
 LRESULT CALLBACK Hook::KeyboardHookProc(int code, WPARAM key, LPARAM kbhook) {
@@ -279,12 +121,9 @@ LRESULT CALLBACK Hook::MouseHookProc(int code, WPARAM mouse, LPARAM lParam) {
   return CallNextHookEx(nullptr, code, mouse, lParam);
 }
 
-LRESULT Hook::WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-
-{
+LRESULT Hook::WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
   switch (msg) {
-    case WM_COMMAND:
-      break;
+    case WM_COMMAND: [[fallthrough]];
     case WM_PAINT:
       break;
     case WM_DESTROY:
@@ -296,29 +135,23 @@ LRESULT Hook::WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
         &dwSize,
         sizeof(RAWINPUTHEADER)); 
 
-      LPBYTE lpbBuffer = new BYTE[dwSize]; 
+      std::unique_ptr<BYTE[]> lpbBuffer { new BYTE[dwSize] };
       GetRawInputData(
-        (HRAWINPUT)lParam, (UINT)RID_INPUT, (LPVOID)lpbBuffer,
+        (HRAWINPUT)lParam, (UINT)RID_INPUT, lpbBuffer.get(),
         (PUINT)&dwSize,
-        (UINT)sizeof(RAWINPUTHEADER));
+        sizeof(RAWINPUTHEADER));
 
-      RAWINPUT *raw = (RAWINPUT *)lpbBuffer;
-      if (raw->header.dwType == RIM_TYPEMOUSE)
-      {
-        EventManager *eventManager =VtuberDelegate::GetInstance()
-            ->GetView()
-            ->GetEventManager();
+      auto raw = reinterpret_cast<RAWINPUT*>(lpbBuffer.get());
+      if (raw->header.dwType == RIM_TYPEMOUSE) {
+        auto manager = VtuberDelegate::GetInstance()->GetView()->GetEventManager();
 
         int xPosRelative = raw->data.mouse.lLastX;
         int yPosRelative = raw->data.mouse.lLastY;
-        eventManager->SetRelativeMouse(xPosRelative,yPosRelative);
+        manager->SetRelativeMouse(xPosRelative, yPosRelative);
 
       }
-      delete[] lpbBuffer;
       break;
     }
-    default:
-      return DefWindowProc(hwnd, msg, wParam, lParam);
   }
   return DefWindowProc(hwnd, msg, wParam, lParam);
 }
@@ -341,22 +174,20 @@ void Hook::Run() {
   hhkLowLevelMs =SetWindowsHookEx(WH_MOUSE_LL, Hook::MouseHookProc, 0, 0);
 
 
-  auto hInst = GetModuleHandle(nullptr);
   WNDCLASSEX wcx {
-    .cbSize = sizeof(wcx)
+    .cbSize = sizeof(wcx),
+    .style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC | CS_NOCLOSE,
+    .lpfnWndProc = WindowProc,
+    .cbClsExtra = 0,
+    .cbWndExtra = 0,
+    .hInstance = GetModuleHandle(nullptr),
+    .hIcon = LoadIcon(nullptr, IDI_APPLICATION),
+    .hCursor = LoadCursor(nullptr, IDC_ARROW),
+    .hbrBackground = static_cast<HBRUSH>(WHITE_BRUSH),
+    .lpszMenuName = nullptr,
+    .lpszClassName = _T("BONGOHOOK"),
+    .hIconSm = nullptr
   };
-  wcx.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC |
-        CS_NOCLOSE;
-  wcx.lpfnWndProc = WindowProc; 
-  wcx.cbClsExtra = 0;           
-  wcx.cbWndExtra = 0;           
-  wcx.hInstance = hInst;        
-  wcx.hIcon = LoadIcon(NULL, IDI_APPLICATION); 
-  wcx.hCursor = LoadCursor(NULL, IDC_ARROW);   
-  wcx.hbrBackground = (HBRUSH)WHITE_BRUSH;     
-  wcx.lpszMenuName = NULL;                     
-  wcx.lpszClassName = _T("BONGOHOOK");
-  wcx.hIconSm = NULL;
 
   if (!RegisterClassEx(&wcx)) {
     printf("RegisterClassEx failed");
@@ -379,7 +210,7 @@ void Hook::Run() {
     300,
     nullptr,
     nullptr,
-    hInst,
+    wcx.hInstance,
     nullptr);
 
   if (!m_hWnd) {
